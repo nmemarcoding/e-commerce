b13ed55ae17b10545909b117c0895ffb6bca13be
nmemarcoding <nmemarcoding@gmail.com>
2022-05-28 12:08:34 -0700

    update order by id rout created

--- api/routes/order.js
+++ api/routes/order.js
@@ -38,4 +38,16 @@ router.get('/:id', async(req, res) => {
     }
 })
 
+// update order by order id 
+router.put('/:id', async(req, res) => {
+    try {
+        const updatedOrder = await Order.findByIdAndUpdate(
+            req.params.id, { $set: req.body }, { new: true }
+        );
+        res.status(200).json(updatedOrder);
+    } catch (err) {
+        res.status(500).json(err.message)
+    }
+})
+
 export default router;
