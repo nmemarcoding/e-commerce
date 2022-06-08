2117fb5460ca4b5255834a1a5ad85032ebca265a
nmemarcoding <nmemarcoding@gmail.com>
2022-06-08 11:32:11 -0700

    Add to basket created

--- client/src/components/itemCard/ItemCard.js
+++ client/src/components/itemCard/ItemCard.js
@@ -2,16 +2,28 @@ import React from 'react'
 import "./ItemCard.css"
 
 export default function ItemCard({items}) {
+    const basket = ()=>{
+        
+
+        if(localStorage.getItem("basket")){
+            const baskets = JSON.parse(localStorage.getItem("basket"))
+            baskets.push(items);
+            window.localStorage.setItem("basket", JSON.stringify(baskets));
+
+        }else{
+            window.localStorage.setItem("basket", JSON.stringify([items]));
+        }
+    }
     return (
         <div className="itemCard">
             <div className="card">
-                <img src={items.photos[0]}></img>
+                <img className="itemCard__image"src={items.photos[0]}></img>
                 <div className="container">
                     <h4><b>{items.name}</b></h4>
                     <p><b>{items.price} $</b></p>
-                    <p>{items.description}</p>
+                    <p className="itemCard__description">{items.description}</p>
                 </div>
-                <button className="addtocard">ADD TO CARD</button>
+                <button className="addtocard"  onClick={basket}>ADD TO CARD</button>
             </div>
             
         
